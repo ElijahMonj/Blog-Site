3d34ff08515af03c5f39f2bc15838758171e4d4a
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-01-02 18:37:11 +0800

    Finished adding comments

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const https = require('https');
-const app = express()
+
+var app = express()
 const request = require('request');
 const data = "http://localhost:3000/blogs"
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
 
@@ -21,7 +22,52 @@ app.get('/', (req, res) => {
     });
     
 })
+app.get('/blog/:blogID',(req,res)=>{
+  console.log(req.params.blogID)
+  let link="http://localhost:3000/blogs/"+req.params.blogID
+  request.get({
+    url:link,
+    json: true
+},(err,response,data)=>{
+   console.log("blog title: "+data.title)
+    //res.send(data.title)
+   res.render("blog",{data: data})
+});
+})
+app.post('/blog/:id/newComment', function(req,res){
+  function date(){
+    var d = new Date();          
+    var n = d.toLocaleString([], { hour12: true});
+    return n
+  }
+  console.log("-------------")
+  console.log(req.params.id)
+  var newComment = [
+    { commentEmail: req.body.commentEmail , commentTime:date(),commentContent: req.body.commentContent},   
+  ];
+  console.log(newComment)
+  let link="http://localhost:3000/blogs/"+req.params.id+"/newComment"
+  request.patch(
+    //First parameter API to make post request
+    link,
 
-app.listen(3001, () => {
-  console.log(`app listening on port 3001`)
+    //The second parameter, DATA which has to be sent to API
+    { json: {
+      commentEmail: req.body.commentEmail , commentTime:date(),commentContent: req.body.commentContent
+    } 
+  },
+    
+    //The third parameter is a Callback function 
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+            console.log(response.statusCode);
+            res.redirect(`/blog/${req.params.id}`)
+        }
+    }
+);
+})
+const port=8080;
+app.listen(port, () => {
+  console.log(`app listening on port `+port)
 })
