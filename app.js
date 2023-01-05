63c7fc4dd7b933b8244fc85c8e2ed5ce9557b411
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-01-05 10:08:58 +0800

    Finished, removed API

--- app.js
+++ app.js
@@ -2,37 +2,74 @@ const express = require('express')
 
 var app = express()
 const request = require('request');
-const data = "http://localhost:3000/blogs"
+const data = "http://localhost:3000/blogs/"
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const mongoose=require('mongoose')
 
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0.3hfbcxb.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser:true,dbName:'Blog-API'})
+const db=mongoose.connection
+
+const  ObjectID = require('mongodb').ObjectId;
 
 app.set("views", __dirname);
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
+
     console.log("-----------")
+    async function getValueAsync() {
+      let coll = mongoose.connection.db.collection("blogs");
+      let data = await coll.find({}).toArray();
+      //console.log(data)
+      res.render("index", { data: data});
+    }
+    getValueAsync()
+
+
+    /* FOR API
     request.get({
-        url:"http://localhost:3000/blogs",
+        url:data,
         json: true
     },(err,response,data)=>{
        console.log(data[0].title)
        res.render("index",{data: data})
     });
-    
+    */
 })
+
 app.get('/blog/:blogID',(req,res)=>{
+
+  /*
   console.log(req.params.blogID)
-  let link="http://localhost:3000/blogs/"+req.params.blogID
+  let link=data+req.params.blogID
   request.get({
     url:link,
     json: true
-},(err,response,data)=>{
+  },(err,response,data)=>{
    console.log("blog title: "+data.title)
     //res.send(data.title)
    res.render("blog",{data: data})
-});
+
+  });
+  */
+  
+  async function run() {
+    try {
+      let coll = mongoose.connection.db.collection("blogs");
+      
+      const query = { _id: ObjectID(req.params.blogID) };
+      const data = await coll.findOne(query);
+      console.log(data)
+      res.render("blog",{data: data})
+    } catch{
+      console.log("error")
+    }
+  }
+  run()
+  
 })
 app.post('/blog/:id/newComment', function(req,res){
   function date(){
@@ -40,13 +77,35 @@ app.post('/blog/:id/newComment', function(req,res){
     var n = d.toLocaleString([], { hour12: true});
     return n
   }
-  console.log("-------------")
-  console.log(req.params.id)
+  console.log("--CHANGING--")
+  async function run() {
+    try {
+      let coll = mongoose.connection.db.collection("blogs");
+      
+      const query = { _id: ObjectID(req.params.id) };
+      let data = await coll.findOne(query);
+      const newComment={ commentEmail: req.body.commentEmail , commentTime:date(),commentContent: req.body.commentContent};
+      
+      console.log(newComment)
+      let newData=data;
+      console.log("Replacing..............")
+      newData.comments.push(newComment)
+      //console.log(newData)
+      let newProd=await coll.replaceOne({ _id: ObjectID(req.params.id)},newData)
+      console.log(newProd)
+    } catch{
+      console.log(" change error")
+    }
+  }
+  run()
+  res.redirect(`/blog/${req.params.id}#commentSection`)
+  
+  /*
   var newComment = [
     { commentEmail: req.body.commentEmail , commentTime:date(),commentContent: req.body.commentContent},   
   ];
   console.log(newComment)
-  let link="http://localhost:3000/blogs/"+req.params.id+"/newComment"
+  let link=data+req.params.id+"/newComment"
   request.patch(
     //First parameter API to make post request
     link,
@@ -65,7 +124,8 @@ app.post('/blog/:id/newComment', function(req,res){
             res.redirect(`/blog/${req.params.id}`)
         }
     }
-);
+    );
+    */
 })
 const port=8080;
 app.listen(port, () => {
