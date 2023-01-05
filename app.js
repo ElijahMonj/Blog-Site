6a5f4b2a57569c9189e5bc4c79f3eed19f9b05b2
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-01-05 19:24:32 +0800

    changed port

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ app.post('/blog/:id/newComment', function(req,res){
     );
     */
 })
-const port=8080;
+const port=process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app listening on port `+port)
 })
